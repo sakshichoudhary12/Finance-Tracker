f244888cdb54f9b60ff420c29e4b1dd46b6b4c64
sakshichoudhary12 <137709066+sakshichoudhary12@users.noreply.github.com>
2024-10-01 21:29:24 +0530

    Add files via upload

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+        <h6 className="text-center">All rights reserved &copy; Sakshi Choudhary</h6>
+      
+    </div>
+  )
+}
+
+export default Footer
